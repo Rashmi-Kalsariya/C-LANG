63360a0f75c6bad8fa6f02eb8b6db6c6e6949a3a
Rashmi-Kalsariya <rashmikalsariya02@gmail.com>
2023-11-05 00:12:24 +0530

    ch1 intro to c

---
+++ ch1/RN6-1-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a=12;
+	int b=6;
+	clrscr();
+	printf("==========================\n");
+	printf("add of %d and %d is = %d\n",a,b,a+b);
+	printf("sub of %d and %d is = %d\n",a,b,a-b);
+	printf("mul of %d and %d is = %d\n",a,b,a*b);
+	printf("div of %d and %d is = %d\n",a,b,a/b);
+	printf("mod of %d and %d is = %d\n",a,b,a%b);
+	printf("==========================");
+	getch();
+}
+
