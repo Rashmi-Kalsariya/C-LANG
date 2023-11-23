a5fcc95337d6e6143707cc09eacf2946d50eb624
Rashmi-Kalsariya <149687777+Rashmi-Kalsariya@users.noreply.github.com>
2023-11-23 19:59:41 +0530

    Add files via upload

---
+++ extra work/RN6-REV.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n,rem,rev=0;
+	clrscr();
+
+	printf("Enter n: ");
+	scanf("%d",&n);
+
+	while(n != 0)
+	{
+		rem=n%10;
+		rev=rev*10+rem;
+		n=n/10;
+	}
+	printf("REVERSE : %d",rev);
+	getch();
+}
