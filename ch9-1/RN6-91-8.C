7f53b145a4b3c091eef5d95be856705f92943b23
Rashmi-Kalsariya <149687777+Rashmi-Kalsariya@users.noreply.github.com>
2023-11-27 23:43:53 +0530

    Add files via upload

---
+++ ch9-1/RN6-91-8.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j,r=1;
+	clrscr();
+
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",r++);
+		}
+		printf("\n");
+	}
+
+	getch();
+}
