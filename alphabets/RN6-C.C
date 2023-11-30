c598f3f05898a45aa02c002bf00eec81ce29eac2
Rashmi-Kalsariya <149687777+Rashmi-Kalsariya@users.noreply.github.com>
2023-12-01 01:02:09 +0530

    Add files via upload

---
+++ alphabets/RN6-C.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+	clrscr();
+
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=4;j++)
+		{
+			if((i==1||i==5) && (j>=2 && j<=4)||
+			   (i>=2 && i<=4) && (j==1))
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+
+	getch();
+}
